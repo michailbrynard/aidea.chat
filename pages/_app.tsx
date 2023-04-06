779ebf388238a402ed40083c021f00df47eeb589
Michail Brynard <mbrynard@gmail.com>
2023-04-06 12:24:22 +0200

    Prevent SSG loading of BrowserSupabaseClient

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,23 @@ import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { createBrowserSupabaseClient, SupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] });
 
 function App({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+  const [supabaseClient, setSupabaseClient] = useState<SupabaseClient<any, "public", any> | null>(null);
+  
+  useEffect(() => {
+    setSupabaseClient(createBrowserSupabaseClient());
+  }, []);
+  
+  if (!supabaseClient) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <SessionContextProvider
       supabaseClient={supabaseClient}
